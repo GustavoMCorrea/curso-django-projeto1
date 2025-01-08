f8716ba9c53bb8af5c0f49e03f09b3fe9a839fc5
Gustavo Mota <88069408+GustavoMCorrea@users.noreply.github.com>
2025-01-08 15:10:22 -0300

    Refactor templates to use base layout and add recipe detail view

--- recipes/views.py
+++ recipes/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'recipes/pages/home.html')
 
+def recipes(request):
+    return render(request, 'recipes/pages/recipe-view.html')
+
 
