c109dec3652fef54ae4454ea7428ad76985ff350
Gustavo Mota <88069408+GustavoMCorrea@users.noreply.github.com>
2025-01-09 15:26:40 -0300

    Fix Recipe model to link category to User instead of Category

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Category(models.Model):
@@ -19,3 +20,4 @@ class Recipe(models.Model):
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
