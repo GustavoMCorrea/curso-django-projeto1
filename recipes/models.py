c62b686e45df5089f56166ac72ce62715170022a
Gustavo Mota <88069408+GustavoMCorrea@users.noreply.github.com>
2025-01-09 15:33:17 -0300

    Refactor Recipe model to use BigAutoField for ID and add migration for initial database setup

--- recipes/models.py
+++ recipes/models.py
@@ -6,7 +6,7 @@ class Category(models.Model):
 
 
 class Recipe(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
     title = models.CharField(max_length=100,default='')
     description = models.CharField(max_length=165,default='')
     slug = models.SlugField()
