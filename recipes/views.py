42db1cd1846c1f4b4b582c5a447fb2fd786a0c99
Gustavo Mota <88069408+GustavoMCorrea@users.noreply.github.com>
2025-01-10 00:24:56 -0300

    Implement category filtering and enhance recipe author display in templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Recipe
 
 from recipes.utils.recipes.factory import make_recipe
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', context={ 'recipes':[make_recipe() for _ in range(10)] })
+    recipes= Recipe.objects.all().order_by('id')
+    return render(request, 'recipes/pages/home.html', context={ 'recipes':recipes })
+
+def category(request, category_id):
+    recipe= Recipe.objects.filter(category__id=category_id).order_by('id')
+    return render(request, 'recipes/pages/home.html', context={ 'recipes':recipe })
 
 def recipes(request,id):
-    return render(request, 'recipes/pages/recipe-view.html', context={ 'recipe':make_recipe(), 'is_detail_page':True })
+    recipe = Recipe.objects.get(id=id)
+    return render(request, 'recipes/pages/recipe-view.html', context={ 'recipe':recipe})
 
 
