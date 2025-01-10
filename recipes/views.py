ea2fd01bd2ae5efc2739ca04eff6b58d85f93e78
Gustavo Mota <88069408+GustavoMCorrea@users.noreply.github.com>
2025-01-10 00:31:03 -0300

    Filter unpublished recipes in home and category views

--- recipes/views.py
+++ recipes/views.py
@@ -5,11 +5,11 @@ from .models import Recipe
 from recipes.utils.recipes.factory import make_recipe
 
 def home(request):
-    recipes= Recipe.objects.all().order_by('id')
+    recipes= Recipe.objects.filter(is_published=True).order_by('id')
     return render(request, 'recipes/pages/home.html', context={ 'recipes':recipes })
 
 def category(request, category_id):
-    recipe= Recipe.objects.filter(category__id=category_id).order_by('id')
+    recipe= Recipe.objects.filter(category__id=category_id, is_published=True).order_by('id')
     return render(request, 'recipes/pages/home.html', context={ 'recipes':recipe })
 
 def recipes(request,id):
