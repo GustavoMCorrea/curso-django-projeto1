603e9f681dd0a550b600b0dfb273f449d39bb2e5
Gustavo Mota <88069408+GustavoMCorrea@users.noreply.github.com>
2025-01-14 13:06:08 -0300

    Refactor recipe tests: consolidate URL and view function tests into separate files

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,21 +1,9 @@
 from django.test import TestCase
-from django.urls import reverse, resolve
+from django.urls import resolve, reverse
 
 from recipes import views
 from recipes.views import home
 
-class RecipeURLsTest(TestCase):
-    def test_recipe_home_url_is_correct(self):
-        home_url = reverse('recipes:home')
-        self.assertEqual(home_url, '/')
-
-    def test_recipe_category_url_is_correct(self):
-        url = reverse('recipes:category', kwargs={'category_id': 1})
-        self.assertEqual(url, '/recipes/category/1/')
-
-    def test_recipe_recipe_url_is_correct(self):
-        url = reverse('recipes:recipe', kwargs={'id': 1})
-        self.assertEqual(url, '/recipes/1/')    
 
 class RecipeViewTest(TestCase):
 
