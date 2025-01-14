34e614a230474ead0687ba2fd653bdbbc4641f02
Gustavo Mota <88069408+GustavoMCorrea@users.noreply.github.com>
2025-01-14 12:39:20 -0300

    Add URL tests for recipe views and verify view functions

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,7 +1,35 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+
+from recipes import views
+from recipes.views import home
 
 class RecipeURLsTest(TestCase):
     def test_recipe_home_url_is_correct(self):
         home_url = reverse('recipes:home')
         self.assertEqual(home_url, '/')
+
+    def test_recipe_category_url_is_correct(self):
+        url = reverse('recipes:category', kwargs={'category_id': 1})
+        self.assertEqual(url, '/recipes/category/1/')
+
+    def test_recipe_recipe_url_is_correct(self):
+        url = reverse('recipes:recipe', kwargs={'id': 1})
+        self.assertEqual(url, '/recipes/1/')    
+
+class RecipeViewTest(TestCase):
+
+    def test_recipe_view_function_is_correct(self):
+        url = reverse('recipes:home')
+        view = resolve(url)
+        self.assertIs(view.func, home)
+
+    def test_category_view_function_is_correct(self):
+        url = reverse('recipes:category', kwargs={'category_id': 1})
+        view = resolve(url)
+        self.assertIs(view.func, views.category)
+
+    def test_detail_view_function_is_correct(self):
+        url = reverse('recipes:recipe', kwargs={'id': 1})
+        view = resolve(url)
+        self.assertIs(view.func, views.recipes)
