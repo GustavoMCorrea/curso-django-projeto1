4dcd05a5a86ef4768d7d79555174905b7db4eaf5
Gustavo Mota <88069408+GustavoMCorrea@users.noreply.github.com>
2025-01-14 14:18:59 -0300

    Add tests for recipe home view: check status code and template usage

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -21,3 +21,11 @@ class RecipeViewTest(TestCase):
         url = reverse('recipes:recipe', kwargs={'id': 1})
         view = resolve(url)
         self.assertIs(view.func, views.recipes)
+
+    def test_recipe_home_view_returns_status_200(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_home_view_loads_correct_template(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertTemplateUsed(response, 'recipes/pages/home.html')
